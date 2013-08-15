ce1df88a6ede365cadb8077f65e0ac124fadcbe4
WanWizard <wanwizard@wanwizard.eu>
2013-08-15 23:49:53 +0200

    removed the bootstap file; added a FuelServiceProvider

--- src/Fuel/Agent/Providers/FuelServiceProvider.php
+++ src/Fuel/Agent/Providers/FuelServiceProvider.php
@@ -8,20 +8,18 @@
  * @link       http://fuelphp.com
  */
 
-namespace Fuel\Agent;
+namespace Fuel\Agent\Providers;
 
 use Fuel\Dependency\ServiceProvider;
 
 /**
- * ServicesProvider class
- *
- * Defines the services published by this namespace to the DiC
+ * FuelPHP ServiceProvider class for this package
  *
  * @package  Fuel\Agent
  *
  * @since  1.0.0
  */
-class ServicesProvider extends ServiceProvider
+class FuelServiceProvider extends ServiceProvider
 {
 	/**
 	 * @var  array  list of service names provided by this provider
@@ -36,7 +34,19 @@ class ServicesProvider extends ServiceProvider
 		// \Fuel\Agent\Agent
 		$this->register('agent', function ($dic, Array $config = array(), $method = 'browscap')
 		{
-			return new Agent($config, $method);
+			// get the agent config
+			$stack = $this->container->resolve('requeststack');
+			if ($request = $stack->top())
+			{
+				$instance = $request->getApplication()->getConfig();
+			}
+			else
+			{
+				$instance = $this->container->resolve('application.main')->getConfig();
+			}
+			$config = \Arr::merge($instance->load('agent', true), $config);
+
+			return $dic->resolve('Fuel\Agent\Agent', array($config, $method));
 		});
 	}
 }
